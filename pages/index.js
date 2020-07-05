99ba0e30236484fec5b11afa1e4e73502ec9b7e2
GGueco <43228146+gerardbrian19@users.noreply.github.com>
2020-07-05 15:55:32 +0800

    added attribution

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,160 @@
+import React, {Component} from 'react'
 import Head from 'next/head'
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default class Index extends Component {
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    render() {
+        return (
+            <div id="App">
+                <Head>
+                    <title>Frontend Mentor | huddle-landing-page-with-curved-sections</title>
+                    <link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicon-32x32.png"/>
+                    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700|Poppins:700&display=swap" rel="stylesheet"/>
+                </Head>
+                <main id="main-content">
+                    <header className="header">
+                        <div className="container">
+                            <nav className="navbar">
+                                <img src="/static/images/logo.svg" alt="Huddle Logo" className="navbar__logo"/>
+                                <a href="#" className="btn">Try it free</a>
+                            </nav>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+                            <div className="header__hero padtop">
+                                <h1 className="header__title">Build The Community Your Fans Will Love</h1>
+                                <p className="header__description">
+                                    Huddle re-imagines the way we build communities. You have a voice, but so does 
+                                    			your audience. Create connections with your users as you engage in genuine discussion.
+                                </p>
+                                <a href="" className="btn btn-pink btn-large">Get Started For Free</a>
+                                <div className="header__illustrations padbot">
+                                    <img src="/static/images/screen-mockups.svg" alt="" className="header__img"/>
+                                    <div className="header__stats padtop">
+                                        <div className="stat-card">
+                                            <p className="large-text">
+                                                <img src="/static/images/icon-communities.svg" alt="Communities" className="card-icon"/>
+                                                1.4k+</p>
+                                            <span className="small-text">Communities Formed</span>
+                                        </div>
+                                        <div className="stat-card">
+                                            <p className="large-text">
+                                                <img src="/static/images/icon-messages.svg" alt="Communities" className="card-icon"/>
+                                                2.7m+</p>
+                                            <span className="small-text">Messages Sent</span>
+                                        </div>
+                                    </div>
+                                </div>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+                            </div>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+                        </div>
+                    </header>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+                    <section id="features" className="section-curved bg-gray">
+                        <div className="container feature-card">
+                            <div className="feature-card__content">
+                                <h2 className="feature-card__title">Grow Together</h2>
+                                <p className="feature-card__desc">Generate meaningful discussion with your audience and build a strong, loyal
+                                    				community. Think of the insightful conversations you miss out on with a feedback form
+                                </p>
+                            </div>
+                            <div className="feature-card__hero">
+                                <img src="/static/images/illustration-grow-together.svg" alt="Grow Together" className="feature-card__img"/>
+                            </div>
+                        </div>
+                    </section>
+                    <section id="features2" className="section-clean">
+                        <div className="container feature-card">
+                            <div className="feature-card__content">
+                                <h2 className="feature-card__title">
+                                    Flowing Conversations
+                                </h2>
+                                <p className="feature-card__desc">You wouldn't paginate a conversation in real life, so why do it online? Our threads have 
+                                    				just-in-time loading for a more natural flow.
+                                </p>
+                            </div>
+                            <div className="feature-card__hero">
+                                <img src="/static/images/illustration-flowing-conversation.svg" alt="Flowing Conversations" className="feature-card__img"/>
+                            </div>
+                        </div>
+                    </section>
+                    <section id="features3" className="section-curved bg-gray section-curved--2">
+                        <div className="container feature-card">
+                            <div className="feature-card__content">
+                                <h2 className="feature-card__title">Your Users</h2>
+                                <p className="feature-card__desc">It takes no time at all to integrate Huddle with your app's authentication solution. This means, 
+                                    				once signed in to your app, your users can start chatting immediately.
+                                </p>
+                            </div>
+                            <div className="feature-card__hero">
+                                <img src="/static/images/illustration-your-users.svg" alt="Grow Together" className="feature-card__img"/>
+                            </div>
+                        </div>
+                    </section>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+                    <section id="getstarted" className="padtop padbot">
+                        <div className="container">
+                            <h2 className="feature-card__title">Ready To Build Your Community?</h2>
+                            <a href="" className="btn btn-large btn-pink">Get Started For Free</a>
+                        </div>
+                    </section>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+                    <footer id="footer" className="footer-curved">
+                        <div className="container">
+                            <div className="footer__item1">
+                                <img src="/static/images/logo.svg" alt="Footer Logo" className="footer-logo"/>
+                                <p className="footer__address">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nulla quam, hendrerit lacinia 
+                                    				vestibulum a, ultrices quis sem.</p>
+                                <p className="footer-text">
+                                    <img src="/static/images/icon-phone.svg" className="footer-icon" alt="Phone"/>
+                                    Phone: +1-543-123-4567
+                                </p>
+                                <p className="footer-text">
+                                    <img src="/static/images/icon-email.svg" className="footer-icon" alt="Phone"/>
+                                    example@huddle.com
+                                </p>
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+                                <ul className="social-links">
+                                    <li>
+                                        <a href="" className="footer-link">
+                                            <i className="fab fa-facebook-square fa-3x"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="" className="footer-link">
+                                            <i className="fab fa-twitter fa-3x"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="" className="footer-link">
+                                            <i className="fab fa-instagram fa-3x"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="footer__item2">
+                                <h3 className="footer__newsletter">Newsletter</h3>
+                                <p className="newsletter__desc">To receive tips on how to grow your community, sign up to our weekly newsletter. We’ll never 
+                                    				send you spam or pass on your email address</p>
+                                <div className="input__container">
+                                    <input type="email" name="email" id="email"/>
+                                    <submit className="btn btn-pink btn-boxy">Subscribe</submit>
+                                </div>
+                            </div>
+                        </div>
+                    </footer>
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
 
-        footer img {
-          margin-left: 0.5rem;
-        }
+                    <p className="attribution">
+                        Challenge by
+                        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+                                  Coded by
+                        <a href="https://github.com/theAspiringDev1" target="_blank">Gerard Gueco</a>.
+                    </p>
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+                </main>
+            </div>
+        )
+    }
 }
